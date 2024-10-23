54278849b4c5704de9af5f4aecf718c8bef6296a
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2024-10-23 12:42:42 -0500

    Tower Targeting update
    Tower now detects targets using a raycast with a set range in the inspector. The rate of fire also now counts down to 0 even if there is no longer an enemy in range so when a enemy enters range, the count down will be at its proper cooldown

--- Summer Game Idea/Assets/Scripts/Towers/Tower.cs
+++ Summer Game Idea/Assets/Scripts/Towers/Tower.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    [SerializeField] private Vector3 range;
-    [SerializeField] private Vector3 center;
+    [SerializeField] private float range;
 
     private string enemyTag = "Enemy";
     [SerializeField] private List<GameObject> enemiesInRange;
 
     [SerializeField] private float attackRate;
-    private float attackCooldown = 0;
+    [SerializeField] private float attackCooldown = 0;
 
     [SerializeField] private GameObject projectile;
     [SerializeField] private Transform barrelPos;
 
     private void Update()
     {
-        if(enemiesInRange.Count == 0) { return; }
+        if (attackCooldown > 0f){ attackCooldown -= Time.deltaTime; }
+
+        if (!EnemyInRange()) { return; }
+
         if(attackCooldown <= 0f)
         {
             Shoot();
@@ -48,7 +50,28 @@ public class Tower : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(center, range);
+        Gizmos.DrawRay(transform.position, Vector3.forward * range);
+    }
+
+    private bool EnemyInRange()
+    {
+        RaycastHit hit;
+        Debug.Log("RaycastCheck");
+        //layer 9 is the projectile layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, range, 9))
+        {
+            if (hit.transform.gameObject.CompareTag("Enemy"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+        else { return false; }
     }
 
     private void Shoot()
